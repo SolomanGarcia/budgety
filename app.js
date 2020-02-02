bb7aefc9f7f6680fb8a67d1ad85c5bc67fe83eec
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-02 10:36:04 -0700

    delete item from budget controller

--- app.js
+++ app.js
@@ -60,6 +60,21 @@ var budgetController = (function () {
             return newItem;
         },
 
+        deleteItem: function(type , id) {
+            var ids, index;
+
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+
+        },
+
         CalculateBudget: function() {
 
             // Calculate total income and expenses
@@ -247,10 +262,10 @@ var controller = (function (budgetCtrl, UICtrl) {
 
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. Delete the item from the datat structure
-
+            budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the UI
 
