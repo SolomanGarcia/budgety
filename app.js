f5d9703c5f867f8dfe9d6ec151d6030204d9177a
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-02 10:45:00 -0700

    delete item from UI

--- app.js
+++ app.js
@@ -161,6 +161,12 @@ var UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        deleteListItem: function(selectorID) {
+
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+        },
+
         clearFields: function() {
             var fields, fieldsArr;
 
@@ -268,9 +274,10 @@ var controller = (function (budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the UI
-
+            UICtrl.deleteListItem(itemID);
 
             // 3. Update and show new budget
+            updateBudget();
 
         }
 
