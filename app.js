be1af27536837d36b201bfe1bc1b716daaa2bb90
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-03 09:29:05 -0700

    improve UI

--- app.js
+++ app.js
@@ -178,6 +178,12 @@ var UIController = (function () {
 
     };
 
+    var NodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
     return {
         getInput: function () {
             return {
@@ -251,12 +257,6 @@ var UIController = (function () {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-            var NodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-
             NodeListForEach(fields, function(current, index) {
 
                 if (percentages[index] > 0) {
@@ -279,6 +279,22 @@ var UIController = (function () {
 
             year = now.getFullYear();
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+               DOMstrings.inputType + ',' +
+               DOMstrings.inputDescription + ',' +
+               DOMstrings.inputValue);
+           
+
+            NodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+
         },
 
         getDOMstrings: function() {
@@ -306,6 +322,8 @@ var controller = (function (budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
     };
 
